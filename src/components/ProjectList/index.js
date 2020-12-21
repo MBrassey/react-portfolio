3f8307fdd804148a37930ee557ae03424d200ed0
MBrassey <luc1d_life@protonmail.com>
2020-12-21 03:09:22 -0700

    updated mouseover

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import Modal from '../Modal';
+import React, { useState } from "react";
+import Modal from "../Modal";
 
 const ProjectList = ({ category }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -7,48 +7,58 @@ const ProjectList = ({ category }) => {
 
   const [projects] = useState([
     {
-      name: 'financial-tracker',
-      category: 'fullstack',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      name: "financial-tracker",
+      category: "fullstack",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
     },
     {
-      name: 'wavSwap',
-      category: 'component',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      name: "wavSwap",
+      category: "component",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
     },
     {
-      name: 'counter.js',
-      category: 'gist',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      name: "counter.js",
+      category: "gist",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
     },
     {
-      name: 'web3 wallet',
-      category: 'blockchain',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      name: "web3 wallet",
+      category: "blockchain",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie",
     },
   ]);
 
-  const currentProjects = projects.filter((project) => project.category === category);
+  const currentProjects = projects.filter(
+    (project) => project.category === category
+  );
 
   const toggleModal = (image, i) => {
     setCurrentProject({ ...image, index: i });
     setIsModalOpen(!isModalOpen);
   };
 
-  const handleOnMouseOver = () => {
-    console.log("over");
-  }
+  const handleMouseOver = (name) => {
+    console.log(name + " over");
+  };
 
-  const handleOnMouseOut = () => {
-    console.log("out");
-  }
+  const handleMouseOut = (name) => {
+    console.log(name + " out");
+  };
 
   return (
     <div>
-      {isModalOpen && <Modal onClose={toggleModal} currentProject={currentProject} />}
+      {isModalOpen && (
+        <Modal onClose={toggleModal} currentProject={currentProject} />
+      )}
       <div className="flex-row">
         {currentProjects.map((image, i) => (
-          <img onMouseOver={handleOnMouseOver} onMouseOut={handleOnMouseOut}
+          <img
+            onMouseEnter={() => handleMouseOver(image.name)}
+            onMouseLeave={() => handleMouseOut(image.name)}
             src={require(`../../assets/small/${category}/${i}.jpg`)}
             alt={image.name}
             className="img-thumbnail mx-1"
