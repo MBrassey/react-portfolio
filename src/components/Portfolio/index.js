cc4cb42582e06b75db7570c1bd5e19782f3103cc
MBrassey <luc1d_life@protonmail.com>
2020-12-22 21:32:31 -0700

    Responsiveness 1

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -5,34 +5,46 @@ function Portfolio() {
   const [projects] = useState([
     {
       name: "galler3",
-      description: "Single-page photography portfolio application that uses React state, props, Hooks, and reusable UI components",
+      description:
+        "Single-page photography portfolio application that uses React state, props, Hooks, and reusable UI components",
       link: "https://mbrassey.github.io/galler3",
       repo: "https://github.com/MBrassey/galler3",
     },
     {
       name: "waviii",
-      description: "Web3 Ethereum Wallet & Swap React Components Inside Social Media Dapp Concept",
+      description:
+        "Web3 Ethereum Wallet & ERC-20 Token Swap React Components Inside Social Media Dapp Concept",
       link: "https://waviii.herokuapp.com/admin/Wallet",
       repo: "https://github.com/MBrassey/waviii.io",
     },
     {
       name: "TurqMelon",
-      description: "Image ranking & tipping platform written in ReactJS, powered by Solidity Smart Contracts",
+      description:
+        "Image ranking & tipping platform written in ReactJS, powered by Solidity Smart Contracts & IPFS",
       link: "https://github.com/MBrassey/TurquoiseMelon",
       repo: "https://github.com/MBrassey/TurquoiseMelon",
     },
     {
       name: "WeatherDash",
-      description: "Weather dashboard concept using OpenWeather API, ipinfo.io API & jQuery/JS/HTML/CSS",
+      description:
+        "Weather dashboard concept using OpenWeather API, ipinfo.io API & jQuery/JS/HTML/CSS",
       link: "https://mbrassey.github.io/OpenWeather-Dashboard",
       repo: "https://github.com/MBrassey/OpenWeather-Dashboard",
     },
     {
-        name: "ThoughtAPI",
-        description: "API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list",
-        link: "https://thoughtstreamapi-mbrassey.herokuapp.com/api/users",
-        repo: "https://github.com/MBrassey/ThoughtStreamAPI",
-      },
+      name: "ThoughtAPI",
+      description:
+        "API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list",
+      link: "https://thoughtstreamapi-mbrassey.herokuapp.com/api/users",
+      repo: "https://github.com/MBrassey/ThoughtStreamAPI",
+    },
+    {
+      name: "TheDevBlog",
+      description:
+        "Full stack CMS type blog application utilizing ORM with Sequelize/MySQL and adhering to the Model-View-Controller paradigm",
+      link: "https://thedevblog-mbrassey.herokuapp.com",
+      repo: "https://github.com/MBrassey/TheDevBlog",
+    },
   ]);
 
   return (
